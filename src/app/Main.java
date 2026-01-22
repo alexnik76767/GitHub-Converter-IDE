25d9cb9b4cba8b964922907bc39d03482bb74ade
alexnik76767 <alexnik76767@gmail.com>
2026-01-22 21:54:31 +0200

    Add    System.out.println("Version 1.0 ");

--- src/app/Main.java
+++ src/app/Main.java
@@ -9,6 +9,7 @@ public class Main {
     static void main(String[] args) {
 
         System.out.println("Converter App.");
+        System.out.println("Version 1.0 ");
 
         double kgs=5;
         double pnds=12;
